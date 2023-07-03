401f9cdb208bdf88debc113cd3d2b9de1ce46a7a
Henry Moore <henrygerardmoore@gmail.com>
2023-07-03 00:33:19 -0600

    fixed angle issues (for real this time), added boid mouse attraction, changed some default params

--- boids.cpp
+++ boids.cpp
@@ -32,9 +32,12 @@ T RadToDeg(T val) {
 }
 
 float FixAngle(float o) {
-  auto fixed_angle = float(std::fmod(o, 2 * M_PI));
-  if (fixed_angle > M_PI) {
-    fixed_angle -= 2 * M_PI;
+  float fixed_angle = o;
+  if (fixed_angle < 0) {
+    fixed_angle -= 2 * float(M_PI * (floor(fixed_angle / (2 * M_PI))));
+  }
+  if (fixed_angle > 2 * M_PI) {
+    fixed_angle -= 2 * float(M_PI * (floor(fixed_angle / (2 * M_PI))));
   }
   return fixed_angle;
 }
@@ -62,17 +65,23 @@ struct Rates {
   float omega;  // angular velocity
 };
 
-float Distance(State s1, State s2) {
-  return float(std::sqrt(std::pow(s1.x - s2.x, 2) + std::pow(s1.y - s2.y, 2)));
-}
-
-float const kBOID_SEPARATION_WEIGHT = 1;
-float const kBOID_ALIGNMENT_WEIGHT = 1;
-float const kBOID_COHESION_WEIGHT = 1;
+float const kBOID_SEPARATION_WEIGHT =
+    200;  // how far boids want to stay from one another
+float const kBOID_ALIGNMENT_WEIGHT =
+    1;  // how strongly boids want to align with one another
+float const kBOID_COHESION_WEIGHT =
+    0.1;  // how much boids want to go to the center of the flock
+float const kBOID_ATTRACTION_WEIGHT =
+    2;  // how much boids want to go toward the mouse cursor
+float const kBOID_SPEED_FACTOR =
+    0.05;  // how much boids will speed up or slow down
+float const kBOID_MAX_SPEED_DIFF = 60;  // pixels / s
+bool Attraction = true;
 
 class Boid {
  public:
-  explicit Boid(State s) : s_(s) {
+  explicit Boid(State s, std::shared_ptr<sf::RenderWindow> window)
+      : s_(s), window_(std::move(window)) {
     shape_ = sf::ConvexShape();
     shape_.setPointCount(4);
     shape_.setPoint(0, sf::Vector2f(-1, 0));
@@ -80,7 +89,8 @@ class Boid {
     shape_.setPoint(2, sf::Vector2f(1, 0));
     shape_.setPoint(3, sf::Vector2f(0, 1));
     shape_.setFillColor(
-        sf::Color(ColorSampler(RNG)));  // set this boid to a random color :)
+        sf::Color(ColorSampler(RNG) |
+                  255u));  // set this boid to a random color with full alpha
     shape_.setScale(10, 10);
   }
   [[nodiscard]] sf::Shape const& getShape() {
@@ -97,9 +107,9 @@ class Boid {
 
   void updateRates(std::vector<std::shared_ptr<Boid>> const& boids) {
     sf::Vector2f separation_vector{0, 0};
+    sf::Vector2f alignment_vector{0, 0};
     sf::Vector2f cohesion_vector{0, 0};
     sf::Vector2f other_boid_position;
-    float theta_desired_alignment = 0;
     sf::Vector2f const this_boid_position{s_.x, s_.y};
     std::size_t boids_seen = 0;
     for (auto const& boid : boids) {
@@ -108,7 +118,13 @@ class Boid {
         continue;
       }
       auto const other_boid_state = boid->getState();
-      auto const d = Distance(other_boid_state, s_);
+
+      other_boid_position.x = other_boid_state.x;
+      other_boid_position.y = other_boid_state.y;
+      auto const dp = other_boid_position - this_boid_position;
+
+      auto const d = std::sqrt(dp.x * dp.x + dp.y * dp.y);
+
       if (d > vision_distance_) {
         // skip if we can't see this boid
         continue;
@@ -119,13 +135,14 @@ class Boid {
       other_boid_position.y = other_boid_state.y;
 
       // separation
-      separation_vector += 1 / d * (this_boid_position - other_boid_position);
+      separation_vector += 1 / (std::sqrt(d) + 1) * -(dp / d);
 
       // alignment
-      theta_desired_alignment += other_boid_state.o;
+      alignment_vector += sf::Vector2f(std::cos(other_boid_state.o),
+                                       std::sin(other_boid_state.o));
 
       // cohesion
-      cohesion_vector += other_boid_position - this_boid_position;
+      cohesion_vector += dp;
     }
 
     if (boids_seen == 0) {
@@ -134,79 +151,97 @@ class Boid {
       return;
     }
 
-    // normalize
-    separation_vector /= float(boids_seen);
-    theta_desired_alignment /= float(boids_seen);
-    cohesion_vector /= float(boids_seen);
+    ++boids_seen;  // increment this to count the current boid in the weighted
+                   // average
 
-    // sum contributions for desired rotation rate
-    auto const theta_desired_separation =
-        std::atan2(separation_vector.y, separation_vector.x);
-    auto const theta_desired_cohesion =
-        std::atan2(cohesion_vector.y, cohesion_vector.x);
-    auto const theta_change =
-        FixAngle((kBOID_SEPARATION_WEIGHT * theta_desired_separation +
-                  kBOID_ALIGNMENT_WEIGHT * theta_desired_alignment +
-                  kBOID_COHESION_WEIGHT * theta_desired_cohesion) /
-                     (kBOID_SEPARATION_WEIGHT + kBOID_ALIGNMENT_WEIGHT +
-                      kBOID_COHESION_WEIGHT) -
-                 s_.o);
+    // normalize and weight
+    separation_vector *= kBOID_SEPARATION_WEIGHT / float(boids_seen);
+    alignment_vector *= kBOID_ALIGNMENT_WEIGHT / float(boids_seen);
+    cohesion_vector *= kBOID_COHESION_WEIGHT / float(boids_seen);
+
+    sf::Vector2f mouse_coords_rel = {0, 0};
+    if (Attraction) {
+      // if enabled, calculate mouse attraction component
+      auto const mouse_coords =
+          window_->mapPixelToCoords(sf::Mouse::getPosition(*window_));
+
+      auto const window_size = window_->getSize();
+
+      // don't chase the mouse if it's outside the window
+      if (window_->hasFocus() && 0 <= mouse_coords.x &&
+          mouse_coords.x <= float(window_size.x) && 0 <= mouse_coords.y &&
+          mouse_coords.y <= float(window_size.y)) {
+        mouse_coords_rel = mouse_coords - this_boid_position;
+      }
+    }
+
+    auto d_mouse = std::sqrt(mouse_coords_rel.x * mouse_coords_rel.x +
+                             mouse_coords_rel.y * mouse_coords_rel.y);
+    if (d_mouse == 0) {
+      d_mouse = 1;
+    }
+
+    // calculate sum of vectors
+    auto const desired_position_rel =
+        separation_vector + alignment_vector + cohesion_vector +
+        kBOID_ATTRACTION_WEIGHT * (mouse_coords_rel / d_mouse);
+
+    // angle to desired position, relative to current angle
+    auto desired_angle_rel =
+        std::atan2(desired_position_rel.y, desired_position_rel.x) - s_.o;
+
+    // if the angle wraps around, fix it
+    if (desired_angle_rel > M_PI) {
+      desired_angle_rel -= 2 * M_PI;
+    } else if (desired_angle_rel < -M_PI) {
+      desired_angle_rel += 2 * M_PI;
+    }
 
     turn_rate_ = std::min(
-        std::max(-max_turn_rate_, theta_change),
+        std::max(-max_turn_rate_, desired_angle_rel),
         max_turn_rate_);  // clip turn rate to [-max_turn_rate_, max_turn_rate_]
 
-    // calculate desired speed
-    auto const desired_position =
-        (kBOID_SEPARATION_WEIGHT * separation_vector +
-         kBOID_COHESION_WEIGHT * cohesion_vector) /
-        (kBOID_SEPARATION_WEIGHT + kBOID_COHESION_WEIGHT);
-    auto const angle_to_desired_position = FixAngle(
-        std::atan2(desired_position.y - s_.y, desired_position.x - s_.x) -
-        s_.o);
-
     auto const distance_to_desired_position =
-        float(std::sqrt(std::pow(desired_position.y - s_.y, 2) +
-                        std::pow(desired_position.x - s_.x, 2)));
+        float(std::sqrt(std::pow(desired_position_rel.y, 2) +
+                        std::pow(desired_position_rel.x, 2)));
 
-    speed_ =
-        std::cos(angle_to_desired_position) * distance_to_desired_position /
-            vision_distance_ * max_speed_diff_ +
-        BoidAverageSpeed;  // if the desired location is as far as we can see
-                           // directly behind us, go min speed; If it's as far
-                           // as we can see in front of us, go max speed
-    speed_ = std::min(std::max(BoidAverageSpeed - max_speed_diff_, speed_),
-                      BoidAverageSpeed + max_speed_diff_);
+    speed_ = kBOID_SPEED_FACTOR * std::cos(desired_angle_rel) *
+                 distance_to_desired_position * kBOID_MAX_SPEED_DIFF +
+             BoidAverageSpeed;  // move faster if we need to catch up, slower if
+                                // we are ahead
+    speed_ = std::min(std::max(BoidAverageSpeed - kBOID_MAX_SPEED_DIFF, speed_),
+                      BoidAverageSpeed + kBOID_MAX_SPEED_DIFF);
   }
 
  private:
   State s_;
   sf::ConvexShape shape_;
-  float vision_distance_ = 100;       // pixels
+  std::shared_ptr<sf::RenderWindow> window_;
+  float vision_distance_ = 300;       // pixels
   float const max_turn_rate_ = M_PI;  // radians / s
   float turn_rate_ = 0;
-  float speed_ = BoidAverageSpeed;   // pixels / s
-  float const max_speed_diff_ = 20;  // pixels / s
+  float speed_ = BoidAverageSpeed;  // pixels / s
 };
 
 int main() {
-  auto window = sf::RenderWindow{{1920u, 1080u}, "Boids"};
+  auto const window = std::make_shared<sf::RenderWindow>(
+      sf::VideoMode(1920u, 1080u), std::string("SFML"));
   unsigned int const desired_framerate = 165;
   float dt = 1 / float(desired_framerate);
   bool paused = false;
   float turn_bias = 0;
 
-  window.setFramerateLimit(desired_framerate);
+  window->setFramerateLimit(desired_framerate);
   std::vector<std::shared_ptr<Boid>> boids;
-  while (window.isOpen()) {
-    for (auto event = sf::Event{}; window.pollEvent(event);) {
+  while (window->isOpen()) {
+    for (auto event = sf::Event{}; window->pollEvent(event);) {
       if (event.type == sf::Event::Closed) {
-        window.close();
+        window->close();
       } else if (event.type == sf::Event::MouseButtonPressed) {
         auto const coords =
-            window.mapPixelToCoords(sf::Mouse::getPosition(window));
+            window->mapPixelToCoords(sf::Mouse::getPosition(*window));
         boids.emplace_back(std::make_shared<Boid>(
-            State(coords.x, coords.y, AngleSampler(RNG), window)));
+            State(coords.x, coords.y, AngleSampler(RNG), *window), window));
       } else if (event.type == sf::Event::KeyPressed) {
         if (event.key.code == sf::Keyboard::Key::Space) {
           paused = !paused;
@@ -217,11 +252,13 @@ int main() {
           BoidAverageSpeed = std::min(300.f, BoidAverageSpeed);
         } else if (event.key.code == sf::Keyboard::Key::Dash) {
           BoidAverageSpeed -= 10;
-          BoidAverageSpeed = std::max(10.f, BoidAverageSpeed);
+          BoidAverageSpeed = std::max(kBOID_MAX_SPEED_DIFF, BoidAverageSpeed);
         } else if (event.key.code == sf::Keyboard::Key::Left) {
           turn_bias = -1;
         } else if (event.key.code == sf::Keyboard::Key::Right) {
           turn_bias = 1;
+        } else if (event.key.code == sf::Keyboard::Key::A) {
+          Attraction = !Attraction;
         }
       } else if (event.type == sf::Event::KeyReleased) {
         if (event.key.code == sf::Keyboard::Left ||
@@ -230,7 +267,7 @@ int main() {
         }
       }
     }
-    window.clear();
+    window->clear();
     for (auto const& boid : boids) {
       auto const& state = boid->getState();
       boid->updateRates(boids);
@@ -242,13 +279,13 @@ int main() {
         auto const new_x = state.x + std::cos(state.o) * boid_speed * dt;
         auto const new_y = state.y + std::sin(state.o) * boid_speed * dt;
 
-        auto const wrapped_state = State(new_x, new_y, new_o, window);
+        auto const wrapped_state = State(new_x, new_y, new_o, *window);
 
         boid->setState(wrapped_state);
       }
 
-      window.draw(boid->getShape());
+      window->draw(boid->getShape());
     }
-    window.display();
+    window->display();
   }
 }
