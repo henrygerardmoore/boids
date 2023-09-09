7445b7355ab29483e0f5a136dd6c2a3b6c8fbd06
Henry Moore <henrygerardmoore@gmail.com>
2023-09-08 18:57:46 -0600

    fixed order of wmctrl commands

--- boid_desktop_bg.sh
+++ boid_desktop_bg.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 ./build/boids &
 sleep 0.5
-wmctrl -F -r boid_desktop_bg -b add,below
 wmctrl -F -r boid_desktop_bg -b add,skip_taskbar
+wmctrl -F -r boid_desktop_bg -b add,below
