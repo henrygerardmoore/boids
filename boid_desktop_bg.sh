27fa2eebdf7b610eb7730a29ce6e631fdc22f474
Henry Moore <henrygerardmoore@gmail.com>
2024-09-15 14:54:57 -0600

    update to conan2

--- boid_desktop_bg.sh
+++ boid_desktop_bg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-./build/boids &
+./build/Release/boids &
 sleep 2
 wmctrl -F -r boid_desktop_bg -b add,skip_taskbar
 wmctrl -F -r boid_desktop_bg -b add,below
--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,40 @@
 # -*- coding: utf-8 -*-
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
 class GameConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = ("opengl/system", "sfml/2.5.1", "spdlog/1.10.0", "nlohmann_json/3.11.2")
-    generators = "cmake_find_package", "cmake_paths"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    def requirements(self):
+        self.requires("opengl/system")
+        self.requires("sfml/2.5.1")
+        self.requires("spdlog/1.10.0")
+        self.requires("nlohmann_json/3.11.2")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.options["sfml/*"].audio = False
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
         cmake.build()
